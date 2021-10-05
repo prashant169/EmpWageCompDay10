09ae479e066cd0ad3e3ff496c9ce50bcea2ac4bf
prashant169 <prashantphad2018@gmail.com>
2021-10-05 00:37:35 -0800

    Calculate Wage Using Till Condition

--- EmpWageComp.java
+++ EmpWageComp.java
@@ -1,43 +1,64 @@
 package com.employeewage;
- 
+
 import java.util.Random;
-  
-public class  EmpWageComp {
-        public static void main( String []args )
-        {
-                System.out.println("Welcome");
-                dhoni();
-        }
 
-                static void dhoni(){
-                                        int TotalWorkingHour = 8 ;
-                                        int TotalWorkingDay  = 20 ;
-                                        int RatePerHour = 20 ;
-                                        
-                                        Random rn = new Random();
-                                        int rand = rn.nextInt(3);
-                                        if (rand == 0)
-                                        {
-                                                System.out.println("----------This is Full time Work in Month---------");
-                                                int TotaEmployeeWage = TotalWorkingHour * TotalWorkingDay * RatePerHour ;
-                                                System.out.println("Total Employee Wages is: " + TotaEmployeeWage);
-                                        }
-                                        else if (rand == 0)
-                                        {
-                                                System.out.println("-------This is Part Time Work in Month--------");
-                                                int TotaEmployeeWage = TotalWorkingHour * TotalWorkingDay * RatePerHour  ;
-                                                System.out.println("Total Employee Wages is: "+ TotaEmployeeWage);
-                                        }
-                                        else
-                                        {
-                                                System.out.println("----Employee is Absent------");
-                                        }
+public class EmpWageComp {
 
+	public static final int FullDayHour = 8;
+	public static final int WorkingDayMonth = 20;
+	public static final int maxHoursInMonth = 100;
+	public static final int perHour = 20;
 
+	public static void main(String x[]) {
 
-                           }
+		MonthlyWages();
+	}
 
+	static void MonthlyWages() {
+		int fullTimeDays = 0;
+		int partTimeDays = 0;
+		int totalempHour = 0;
+		int totalWorkingDay = 0;
+		int absent = 0;
 
+		while (maxHoursInMonth > totalempHour && WorkingDayMonth > totalWorkingDay) {
+
+			totalWorkingDay++;
+
+			Random rn = new Random();
+			int rand = rn.nextInt(3);
+
+			switch (rand) {
+
+			case 0:
+				fullTimeDays++;
+				totalempHour = totalempHour + FullDayHour;
+				break;
+			case 1:
+				partTimeDays++;
+				totalempHour = totalempHour + (FullDayHour / 2);
+				break;
+			default:
+				absent++;
+				break;
+
+			}
+		}
+		int FullTimeSalary = perHour * (FullDayHour * fullTimeDays);
+		int PartTimeSalary = (partTimeDays * (FullDayHour / 2)) * perHour;
+
+		System.out.println("------------Employee_Wages-------------");
+		System.out.println("---------------------------------------");
+		System.out.println("FullTime_Days_in_month: " + fullTimeDays);
+		System.out.println("PartTime_Days_in_month: " + partTimeDays);
+		System.out.println("Absent_Days_in_month: " + absent);
+		System.out.println("Total_working_Days_IN_month : " + (fullTimeDays + partTimeDays));
+		System.out.println("Total_Hours    :===> " + totalempHour);
+		System.out.println("FullTime_Salary:===> " + FullTimeSalary);
+		System.out.println("PartTime_Salary:===> " + PartTimeSalary);
+		System.out.println("Total_Salary   :===> " + (FullTimeSalary + PartTimeSalary));
+		System.out.println("---------------------------------------");
+		System.out.println(totalWorkingDay);
+
+	}
 }
-
-
